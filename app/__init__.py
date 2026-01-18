5f336641e6d5dd01acc80f0cb84e9991fa88d02c
agung_win_22 <134590738+agungwin22@users.noreply.github.com>
2026-01-18 11:10:59 +0700

    Delete app/__init__.py

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from config import Config
-from app.routes import main
-
-def mainloop():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    app.register_blueprint(main)
-    return app
